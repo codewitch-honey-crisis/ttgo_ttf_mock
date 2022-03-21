a83c55786d80f231159cca7029099b1f676ea31c
codewitch-honey-crisis <gaztoast@gmail.com>
2022-03-21 14:27:57 -0700

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,58 @@
+#define LCD_WIDTH 135
+#define LCD_HEIGHT 240
+#define LCD_HOST    VSPI
+#define PIN_NUM_MISO -1
+#define PIN_NUM_MOSI 19
+#define PIN_NUM_CLK  18
+#define PIN_NUM_CS   5
+
+#define PIN_NUM_DC   16
+#define PIN_NUM_RST  23
+#define PIN_NUM_BCKL 4
+
+#include <Arduino.h>
+#include <gfx_cpp14.hpp>
+#include <st7789.hpp>
+#include "test.h"
+using namespace arduino;
+using namespace gfx;
+
+using bus_type = tft_spi_ex<VSPI,PIN_NUM_CS,PIN_NUM_MOSI,PIN_NUM_MISO,PIN_NUM_CLK,SPI_MODE0,
+true
+#ifdef OPTIMIZE_DMA
+,LCD_WIDTH*LCD_HEIGHT*2+8
+#endif
+>;
+
+using lcd_type = st7789<LCD_WIDTH,LCD_HEIGHT,PIN_NUM_DC,PIN_NUM_RST,PIN_NUM_BCKL,bus_type,1>;
+
+lcd_type lcd;
+char text[256];
+using lcd_color = color<typename lcd_type::pixel_type>;
+
+open_font_cache fcache;
+int counter;
+float scale;
+const open_font& test_font = test_ttf;
+uint32_t counter_ts;
+const int HZ = 25;
+void setup() {
+  Serial.begin(115200);
+  counter = 0;
+  counter_ts = 0;
+  scale = test_ttf.scale(50);
+  draw::filled_rectangle(lcd,(srect16)lcd.bounds(),lcd_color::white);
+}
+
+void loop() {
+  const uint32_t ms = millis();
+    if(ms>counter_ts+(1000/HZ)) {
+      ssize16 sz = test_font.measure_text((ssize16)lcd.dimensions(),{0,0},itoa(counter,text,10),scale,0,gfx_encoding::utf8,&fcache);
+      srect16 sr = sz.bounds().center((srect16)lcd.bounds());
+      draw::filled_rectangle(lcd,sr,lcd_color::white);
+      draw::text(lcd,sr,{0,0},text,test_font,scale, lcd_color::blue,lcd_color::white,false,false,0,gfx_encoding::utf8,nullptr,&fcache);
+      ++counter;
+      counter_ts = millis();
+      
+    }
+}
